942f8cf581d45188efc9933fb54294499e2a89cc
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-10 19:25:23 +0530

    backend updated

--- server/server.js
+++ server/server.js
@@ -24,7 +24,14 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     try {
       const prompt = req.body.prompt;
-  
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1, 
+        presence_penalty: 0, 
+      });
   
       res.status(200).send({
         bot: response.data.choices[0].text
